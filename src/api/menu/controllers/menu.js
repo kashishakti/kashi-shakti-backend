881692cc01826ea9038431e7e6a2435c7cbec63b
coldKaushal <kauagg111@gmail.com>
2026-06-13 23:19:15 +0530

    add pagination to menu api controller

--- src/api/menu/controllers/menu.js
+++ src/api/menu/controllers/menu.js
@@ -5,6 +5,7 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { getPagination, setPaginationHeaders } = require('../../../utils/pagination');
 
 const populate = {
     // shared.link has no media/relations
@@ -15,34 +16,38 @@ module.exports = createCoreController('api::menu.menu', ({ strapi }) => ({
 
     // 🔹 GET ALL MENUS
     async find(ctx) {
-        const data = await strapi.entityService.findMany(
-            'api::menu.menu',
-            {
+        const { page, pageSize, start, limit } = getPagination(ctx);
+        const [data, total] = await Promise.all([
+            strapi.entityService.findMany('api::menu.menu', {
                 populate,
                 sort: { createdAt: 'asc' },
+                start,
+                limit,
+            }),
+            strapi.entityService.count('api::menu.menu'),
+        ]);
+
+        setPaginationHeaders(ctx, page, pageSize, total);
+        ctx.body = data;
+    },
+
+    // 🔹 GET MENU BY ID
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const data = await strapi.entityService.findOne(
+            'api::menu.menu',
+            id,
+            {
+                populate,
             }
         );
 
-    ctx.body = data;
-  },
+        if (!data) {
+            return ctx.notFound(`Menu with id "${id}" not found`);
+        }
 
-  // 🔹 GET MENU BY ID
-  async findOne(ctx) {
-    const { id } = ctx.params;
-
-    const data = await strapi.entityService.findOne(
-      'api::menu.menu',
-      id,
-      {
-        populate,
-      }
-    );
-
-    if (!data) {
-      return ctx.notFound(`Menu with id "${id}" not found`);
-    }
-
-    ctx.body = data;
-  },
+        ctx.body = data;
+    },
 
 }));
